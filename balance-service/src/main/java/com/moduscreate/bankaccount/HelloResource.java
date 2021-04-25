a65504a909a86c92076b4791bafdcf9e68440666
Vinicius Escame <v.escame@gmail.com>
2021-04-25 18:44:41 -0300

    Fix hello resource missing and failing tests   - update postgres prod references

---
+++ balance-service/src/main/java/com/moduscreate/bankaccount/HelloResource.java
@@ -0,0 +1,18 @@
+package com.moduscreate.bankaccount;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class HelloResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response hello() {
+        return Response.ok("hello").build();
+    }
+}
+
